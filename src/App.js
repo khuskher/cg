f1204e64eec379b6a12a17f0c6a8b3ccbd13d1e0
khuskher <KHUS.KHER@GMAIL.COM>
2023-07-12 21:32:55 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Post from './pages/Report';
+import Feed from './pages/Feed';
+import NoPage from './pages/NoPage';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route } from "react-router-dom";
+import { Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <div style={{width: '100%'}}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/report" component={Post} />
+          <Route path="/feed" component={Feed} />       
+
+          <Route path="/error" component={NoPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+
   );
 }
-
 export default App;
